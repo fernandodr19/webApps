7f033f31b9f410815b25606f82487a2214857ffb
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2018-05-07 01:02:05 -0300

    Add heatmap

--- googleMaps/app.js
+++ googleMaps/app.js
@@ -4,6 +4,7 @@ function initMap() {
     initDirectionMap();
     initMarkerMap();
     initCircleMarkMap();
+    initHeatMap();
 }
 
 function initBaseMap() {
@@ -95,7 +96,36 @@ function getCircle(magnitude) {
     };
 }
 
+var heatMap;
+function initHeatMap() {
+    heatMap = new google.maps.Map(document.getElementById('heatMap'), {
+      zoom: 2,
+      center: {lat: -33.865427, lng: 151.196123},
+      mapTypeId: 'terrain'
+    });
+
+    // Create a <script> tag and set the USGS URL as the source.
+    var script = document.createElement('script');
+
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+}
+
 function eqfeed_callback(results) {
     circleMarkMap.data.addGeoJson(results);
+    
+    var heatmapData = [];
+    for (var i = 0; i < results.features.length; i++) {
+      var coords = results.features[i].geometry.coordinates;
+      var latLng = new google.maps.LatLng(coords[1], coords[0]);
+      heatmapData.push(latLng);
+    }
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatmapData,
+      dissipating: false,
+      map: heatMap
+    });
 }
 
